e61a6771ca2ecc6ad8500bea7ff99de41cd9daad
DumitriuAndrei <72802470+DumitriuAndrei@users.noreply.github.com>
2024-05-24 17:49:47 +0300

    app

---
+++ components/main-header/main-header.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Image from "next/image"
+import logoImg from "@/assets/logo.png";
+
+import classes from '@/components/main-header/main-header.module.css';
+import MainHeaderBackground from "@/components/main-header/main-header-background";
+import NavLink from "@/components/main-header/nav-link";
+
+export default function MainHeader() {
+    return (
+        <>
+            <MainHeaderBackground/>
+            <header className={classes.header}>
+                <Link href="/" className={classes.logo}>
+                    <Image src={logoImg} alt="A plate with food on it" priority/>
+                    NextLevel food
+                </Link>
+
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <NavLink href="/meals">
+                                Browse meals
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink href="/community">
+                                Foodies community
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
